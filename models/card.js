0bc45fec38bca7c9355545eaa686f32be06391b4
DCarr40 <davidwcarruthers@yahoo.com>
2021-06-30 18:55:42 -0400

    refactored cardSchema in card.js model

--- models/card.js
+++ models/card.js
@@ -2,23 +2,16 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 const cardSchema = new mongoose.Schema({
-
-    name: {type: String, required: true, minLength:2, maxLength:255},
-    description: {type: String, required: true},
-    category: {type: String, required: true, minLength:5, maxLength:50},
-    price: {type: Number, required: true},
-    dateModified: {type: Date, default: Date.now},
-
+    pokemonName: {type: String, required: true},
+    description: {type: String, required: true, minLength:2, maxLength:255},
 });
 
 const Card = mongoose.model('Card',cardSchema);
 
 function validateCard(card) {
     const schema = Joi.object({
-        name: Joi.string().min(2).max(50).required(),
+        pokemonName: Joi.string().min(2).max(50).required(),
         description: Joi.string().required(),
-        category: Joi.string().min(5).max(50).required(),
-        price: Joi.number().required(),
     });
 
     return schema.validate(card);
